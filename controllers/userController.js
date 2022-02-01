34dc5eaf796b0f124beccdbfc99385b205b5738b
theoverse <theoverse@protonmail.com>
2022-02-01 11:47:24 -0800

    added session management to app

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const User = require('../models/User')
 exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result) {
+        req.session.user = {username: user.data.username}
         res.send(result)
     }).catch(function(err) {
         res.send(err)
@@ -24,5 +25,9 @@ exports.register = function(req, res) {
 }
 
 exports.home = function(req, res) {
-    res.render('home-guest')
+    if (req.session.user) {
+        res.send('Welcome!!!')
+    } else {
+        res.render('home-guest')
+    }
 }
