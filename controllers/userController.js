403ef8e4b58b6449ad7d94644e74315c5e4933d9
theoverse <theoverse@protonmail.com>
2022-02-01 17:36:14 -0800

    modified user login, added login redirect and flash messages

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,26 @@
+const { request } = require('http')
 const User = require('../models/User')
 
 exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result) {
         req.session.user = {username: user.data.username}
-        res.send(result)
+        req.session.save(function() {
+            res.redirect('/')
+        })
     }).catch(function(err) {
-        res.send(err)
+        req.flash('errors', err)
+        req.session.save(function() {
+            res.redirect('/')
+        })
     })
 }
 
-exports.logout = function() {
-
+exports.logout = function(req, res) {
+    req.session.destroy(function() {
+        res.redirect('/')
+    })
+    
 }
 
 exports.register = function(req, res) {
@@ -28,6 +37,6 @@ exports.home = function(req, res) {
     if (req.session.user) {
         res.render('home-dashboard', {username: req.session.user.username})
     } else {
-        res.render('home-guest')
+        res.render('home-guest', {errors: req.flash('errors')})
     }
 }
